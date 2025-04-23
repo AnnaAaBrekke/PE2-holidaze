aa155ac596b42097b6ab1b0bccee1809dfd5aa3e
AnnaAaBrekke <annabrekke98@gmail.com>
2025-04-23 09:48:51 +0200

    Fetch venue list on home page and style somehwat using figma

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import VenueList from "../../components/Venue/VenueList";
 
 const Home = () => {
   return (
     <div>
       <h1>Home Page</h1>
+      <VenueList />
     </div>
   );
 };
