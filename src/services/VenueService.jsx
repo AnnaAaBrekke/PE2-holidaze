78c85491c3f1738345ae34c438e1ec6be5c60e87
AnnaAaBrekke <annabrekke98@gmail.com>
2025-04-24 14:36:45 +0200

    Add all the api services for venues crud

---
+++ src/services/VenueService.jsx
@@ -0,0 +1,24 @@
+import apiFetch from "../utils/apiFetch";
+
+export const createVenue = async (venueData, token) => {
+  return await apiFetch("venues", {
+    method: "POST",
+    token,
+    body: venueData,
+  });
+};
+
+export const updateVenue = async (id, venueData, token) => {
+  return await apiFetch(`/venues/${id}`, {
+    method: "PUT",
+    token,
+    body: venueData,
+  });
+};
+
+export const deleteVenue = async (id, token) => {
+  return await apiFetch(`/venues/${id}`, {
+    method: "DELETE",
+    token,
+  });
+};
