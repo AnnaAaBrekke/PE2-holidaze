b2b4cfc6be9ce36cd3340b20bdff0f8899d9d07c
AnnaAaBrekke <annabrekke98@gmail.com>
2025-04-27 10:46:32 +0200

    installed react-debounce-input:  local client-side search filtering was implemented

--- src/components/Venue/VenueList.jsx
+++ src/components/Venue/VenueList.jsx
@@ -1,17 +1,45 @@
+import { useState } from "react";
 import useVenues from "../../hooks/useVenues";
 import VenueCard from "./VenueCard";
+import { DebounceInput } from "react-debounce-input";
 
 const VenuesList = () => {
   const { venues, loading, error } = useVenues();
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const searchLowerCase = searchTerm.toLowerCase();
+
+  const filteredVenues = venues.filter(({ name = "", description = "" }) => {
+    return (
+      name.toLowerCase().includes(searchLowerCase) ||
+      description.toLowerCase().includes(searchLowerCase)
+    );
+  });
 
   if (loading) return <p>Loading venues...</p>;
   if (error) return <p>Error: {error}</p>;
 
   return (
-    <div className="grid gap-10 sm:grid-cols-2 lg:grid-cols-3 justify-items-center">
-      {venues.map((venue) => (
-        <VenueCard key={venue.id} venue={venue} />
-      ))}
+    <div>
+      <div>
+        <DebounceInput
+          minLength={2}
+          debounceTimeout={300}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search Venues..."
+          className="border border-gray w-full m-2"
+        />
+      </div>
+
+      <div className="grid gap-10 sm:grid-cols-2 lg:grid-cols-3 justify-items-center">
+        {filteredVenues.length > 0 ? (
+          filteredVenues.map((venue) => (
+            <VenueCard key={venue.id} venue={venue} />
+          ))
+        ) : (
+          <p>No venues found.</p>
+        )}
+      </div>
     </div>
   );
 };
