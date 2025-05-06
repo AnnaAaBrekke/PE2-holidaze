7eed1c33c8a1c4349b6961f92850a75c21765bbc
AnnaAaBrekke <annabrekke98@gmail.com>
2025-05-06 09:48:16 +0200

    Refactored  configure ESLint with JSX and Prettier support, ignore dist folder

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  parser: "@babel/eslint-parser",
+  parserOptions: {
+    requireConfigFile: false,
+    babelOptions: {
+      presets: ["@babel/preset-react"],
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["react", "react-hooks", "prettier"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "prettier/prettier": "warn",
+  },
+};
